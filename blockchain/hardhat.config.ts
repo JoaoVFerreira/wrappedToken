1754ebd297e5206d4ebba00279a7f79e463d8ebc
João Ferreira <joaovtf39@gmail.com>
2024-10-24 14:50:16 -0300

    chore: add dependencies

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv"
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
+  networks: {
+    bsctest: {
+      url: process.env.BSC_URL,
+      chainId: parseInt(`${process.env.CHAIN_ID}`),
+      accounts: {
+        mnemonic: process.env.SECRET
+      }
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
+};
+
+export default config;
