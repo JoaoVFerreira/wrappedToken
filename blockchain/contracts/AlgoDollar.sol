6927205ac55eaf4e089f12172d236fb8678f6193
João Ferreira <joaovtf39@gmail.com>
2024-10-28 00:02:36 -0300

    fix: typo in AlgoDollar contract

--- blockchain/contracts/AlgoDollar.sol
+++ blockchain/contracts/AlgoDollar.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IstableCoin.sol";
+import "./IStableCoin.sol";
 
 contract AlgoDollar is ERC20, ERC20Burnable, Ownable, IStableCoin {
   address public rebase;
